407295275655b897df556ba32bae2d642c49ddbd
Andrew Jeffery <andrew@aj.id.au>
2020-01-17 13:59:44 +1030

    wip
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I87427aae44da0dd28a4df0bb428ee39aee598a07

--- tests/test_pktbuf.c
+++ tests/test_pktbuf.c
@@ -14,11 +14,11 @@ struct test_ctx {
 };
 
 struct test_case {
-	int (*fn)(struct mctp_binding *binding);
+	void (*fn)(struct mctp_binding *binding);
 	struct mctp_binding_test binding;
 };
 
-static int test_mctp_pktbuf_alloc(struct mctp_binding *binding)
+static void test_mctp_pktbuf_alloc(struct mctp_binding *binding)
 {
 	struct mctp_pktbuf *pkt;
 
@@ -26,16 +26,45 @@ static int test_mctp_pktbuf_alloc(struct mctp_binding *binding)
 	assert(pkt);
 	assert(mctp_pktbuf_size(pkt) >= binding->pkt_size);
 	mctp_pktbuf_free(pkt);
+}
 
-	return EXIT_SUCCESS;
+static void test_mctp_pktbuf_size(struct mctp_binding *binding)
+{
+	struct mctp_pktbuf *pkt;
+
+	pkt = mctp_pktbuf_alloc(binding, binding->pkt_size);
+	assert(mctp_pktbuf_size(pkt) == binding->pkt_size);
+	mctp_pktbuf_free(pkt);
+}
+
+#include <stdio.h>
+static void test_mctp_pktbuf_alloc_start(struct mctp_binding *binding)
+{
+	struct mctp_pktbuf *pkt;
+	void *a, *b;
+
+	printf("pkt_pad: %d, start: %d\n", binding->pkt_pad);
+	pkt = mctp_pktbuf_alloc(binding, binding->pkt_size);
+
+	a = mctp_pktbuf_data(pkt);
+	b = mctp_pktbuf_alloc_start(pkt, 1);
+	printf("a: %p, b: %p, d: %td\n", a, b, a-b);
+	assert(1 == (a - b));
+
+	mctp_pktbuf_free(pkt);
+}
+
+static void test_mctp_pktbuf_alloc_end(struct mctp_binding *binding)
+{
 }
 
 static const struct test_case tests[] = {
 	{ .fn = test_mctp_pktbuf_alloc, },
+	{ .fn = test_mctp_pktbuf_size, },
 	{
-		.fn = test_mctp_pktbuf_alloc,
+		.fn = test_mctp_pktbuf_alloc_start,
 		.binding.binding = {
-			.pkt_size = 128,
+			.pkt_pad = 1,
 		},
 	},
 };
@@ -47,8 +76,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(tests); i++) {
-		struct test_case *t = &tests[i];
-		struct mctp_binding_test *b = &t->binding;
+		const struct test_case *t = &tests[i];
+		struct mctp_binding_test *b =
+			(struct mctp_binding_test *)&t->binding;
 
 		mctp_test_stack_init(&ctx->mctp, &b, local_eid);
 
