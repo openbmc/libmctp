768e07d40fa91052ee541286022941e1e815969c
Andrew Jeffery <andrew@aj.id.au>
2020-03-10 22:09:02 +1030

    bootstrap: Add a 'dev' mode
    Enables sanitizers and stricter CFLAGS useful for development.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I1db80b5a83c2e155e1007923546b36176481b78a

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,28 @@
 #!/bin/sh
-exec autoreconf -f -i
+
+set -eu
+
+autoreconf -f -i
+
+
+BOOTSTRAP_MODE=""
+
+if [ $# -gt 0 ];
+then
+    BOOTSTRAP_MODE="${1}"
+    shift 1
+fi
+
+case "${BOOTSTRAP_MODE}" in
+    dev)
+        FLAGS="-fsanitize=address,leak,undefined -Wformat -Werror=format-security -Werror=array-bounds -ggdb"
+        ./configure \
+            CFLAGS="${FLAGS}" \
+            CXXFLAGS="${FLAGS}" \
+            --enable-code-coverage \
+            "$@"
+        ;;
+    *)
+        echo 'Run "./configure ${CONFIGURE_FLAGS} && make"'
+        ;;
+esac
