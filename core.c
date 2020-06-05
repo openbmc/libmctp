openbmc/libmctp

commit 0dc1375dbc419739ea732279173dbbbcae8363f7

core: Propagate binding start-up errors in mctp_register_bus()
It's feasible the binding could fail to initialise in some fashion, so
make sure the caller becomes aware.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: If0b0a883c1c6f2004d6bc7b7420cd93d6a6d8fe2

core.c
@@ -284,7 +284,7 @@ int mctp_register_bus(struct mctp *mctp,
 	mctp->route_policy = ROUTE_ENDPOINT;
 
 	if (binding->start)
-		binding->start(binding);
+		return binding->start(binding);
 
 	return 0;
 }

commit 3e8a12a48d446c014939f7c2490c8663733d0ea7

core: Cleanup after failing to start binding
Previously we changed mctp_register_bus() to return the error if the
binding failed to start. That change was broken in that ignored
resources that had been allocated on the path to invoking the binding
start function. Add the necessary cleanup.

Fixes: 0dc1375dbc41 ("core: Propagate binding start-up errors in mctp_register_bus()")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I232242b3be76db9d0c7de60262779cb768173416

core.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
 
 #include <assert.h>
+#include <errno.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -273,10 +274,16 @@ int mctp_register_bus(struct mctp *mctp,
 		struct mctp_binding *binding,
 		mctp_eid_t eid)
 {
+	int rc = 0;
+
 	/* todo: multiple busses */
 	assert(mctp->n_busses == 0);
 	mctp->n_busses = 1;
+
 	mctp->busses = __mctp_alloc(sizeof(struct mctp_bus));
+	if (!mctp->busses)
+		return -ENOMEM;
+
 	memset(mctp->busses, 0, sizeof(struct mctp_bus));
 	mctp->busses[0].binding = binding;
 	mctp->busses[0].eid = eid;
@@ -284,10 +291,16 @@ int mctp_register_bus(struct mctp *mctp,
 	binding->mctp = mctp;
 	mctp->route_policy = ROUTE_ENDPOINT;
 
-	if (binding->start)
-		return binding->start(binding);
+	if (binding->start) {
+		rc = binding->start(binding);
+		if (rc < 0) {
+			mctp_prerr("Failed to start binding: %d", rc);
+			__mctp_free(mctp->busses);
+			mctp->busses = NULL;
+		}
+	}
 
-	return 0;
+	return rc;
 }
 
 int mctp_bridge_busses(struct mctp *mctp,