adbd1679ca0e1d15042ac540b8f76e4cb1cf545a
Andrew Jeffery <andrew@aj.id.au>
2020-06-29 19:50:18 +0930

    test-utils: Augment test binding to stash Tx packets
    Allow users of the binding to inspect packets passing through the
    binding.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: Ib5a38bed314759a84ba58aea2c4578b4e90a0baa

--- tests/test-utils.c
+++ tests/test-utils.c
@@ -1,24 +1,39 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
 
-#include <string.h>
-#include <assert.h>
-
-#include <libmctp.h>
-#include <libmctp-alloc.h>
-
 #include "test-utils.h"
 #include "binding.h"
 
-struct mctp_binding_test {
-	struct mctp_binding	binding;
-};
+#include "container_of.h"
 
-static int mctp_binding_test_tx(struct mctp_binding *b __attribute__((unused)),
-		struct mctp_pktbuf *pkt __attribute__((unused)))
+#include "libmctp.h"
+#include "libmctp-alloc.h"
+
+#include <string.h>
+#include <assert.h>
+
+static int mctp_binding_test_tx(struct mctp_binding *b, struct mctp_pktbuf *pkt)
 {
-	/* we are not expecting TX packets */
-	assert(0);
+	struct mctp_binding_test *test = binding_to_test(b);
+	size_t len;
+
+	len = sizeof(*test->pkt) + mctp_pktbuf_size(pkt);
+	test->pkt = __mctp_alloc(len);
+	assert(test->pkt);
+	memcpy(test->pkt, pkt, len);
+
 	return 0;
+}
+
+static struct mctp_pktbuf *
+mctp_binding_test_frame(struct mctp_binding *b __attribute__((unused)),
+			struct mctp_pktbuf *pkt, const struct mctp_device *dest)
+{
+	uint8_t *hdr;
+
+	hdr = mctp_pktbuf_alloc_start(pkt, 1);
+	*hdr = dest->address & 0xff;
+
+	return pkt;
 }
 
 struct mctp_binding_test *mctp_binding_test_init(void)
@@ -29,9 +44,11 @@ struct mctp_binding_test *mctp_binding_test_init(void)
 	memset(test, '\0', sizeof(*test));
 	test->binding.name = "test";
 	test->binding.version = 1;
+	test->binding.frame = mctp_binding_test_frame;
 	test->binding.tx = mctp_binding_test_tx;
 	test->binding.pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	test->binding.pkt_pad = 0;
+	test->binding.pkt_pad = 1;
+	test->binding.pkt_start = 1;
 	return test;
 }
 
@@ -55,6 +72,7 @@ void mctp_binding_test_register_bus(struct mctp_binding_test *binding,
 		struct mctp *mctp, mctp_eid_t eid)
 {
 	mctp_register_endpoint(mctp, &binding->binding, eid);
+	binding->binding.bus->tx_enabled = 1;
 }
 
 void mctp_test_stack_init(struct mctp **mctp,
