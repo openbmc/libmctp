77384a5ec60b10710a9375f094fbc8c5a95257e9
Sumanth Bhat <sumanth.bhat@linux.intel.com>
2021-05-18 13:16:46 +0530

    core: Ensure sufficient buffer allocation before packet copy
    Currently, context buffers are allocated with 4096 or its 2^n multiples
    before a copy of the packet into context buffer.
    However, if a binding supports large payload transfers, we would
    have to allocate sufficient buffer size for the context buffer.
    
    Tested: WIP
    
    Signed-off-by: Sumanth Bhat <sumanth.bhat@linux.intel.com>
    Change-Id: I0a84cc30dfcfb4fd1b7971a2a17e7b87106865c7

--- core.c
+++ core.c
@@ -229,7 +229,7 @@ static int mctp_msg_ctx_add_pkt(struct mctp_msg_ctx *ctx,
 	if (len > (MAX_CTX_BUFFER_SIZE - ctx->buf_size))
 		return -1;
 
-	if (ctx->buf_size + len > ctx->buf_alloc_size) {
+	while (ctx->buf_size + len > ctx->buf_alloc_size) {
 		size_t new_alloc_size;
 		void *lbuf;
 
