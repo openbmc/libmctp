af0cd6221ba85c01cabd4c3ab463b4547ceaa38a
Moritz Fischer <moritzf@google.com>
2022-06-28 19:00:18 -0700

    range.h: Avoid duplicate definitions of MIN/MAX
    Avoid duplicate definitions of MIN()/MAX() macros in environments
    that already define them
    
    Signed-off-by: Moritz Fischer <moritzf@google.com>
    Change-Id: I1bdb26b54b641218821c059021dcca0062d3d0a3

--- range.h
+++ range.h
@@ -1,18 +1,22 @@
 #ifndef _RANGE_H
 #define _RANGE_H
 
+#ifndef MIN
 #define MIN(a, b)                                                              \
 	({                                                                     \
 		typeof(a) _a = a;                                              \
 		typeof(b) _b = b;                                              \
 		_a < _b ? _a : _b;                                             \
 	})
+#endif
 
+#ifndef MAX
 #define MAX(a, b)                                                              \
 	({                                                                     \
 		typeof(a) _a = a;                                              \
 		typeof(b) _b = b;                                              \
 		_a > _b ? _a : _b;                                             \
 	})
+#endif
 
 #endif
