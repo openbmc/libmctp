99d8f12677e9f3a19d87f132795778c2a4e6de4d
Younghyun Park <younghyunpark@google.com>
2022-07-01 20:09:39 +0000

    range.h: Define typeof with __typeof__
    Depending on toolchains, "typeof" is used instead of "__typeof__".
    This is to avoid a compile error caused by missing "typeof".
    
    Signed-off-by: Younghyun Park <younghyunpark@google.com>
    Change-Id: I9c224151cee32fdb71e1caefb25ea53cc475ff42

--- range.h
+++ range.h
@@ -1,6 +1,10 @@
 #ifndef _RANGE_H
 #define _RANGE_H
 
+#ifndef typeof
+#define typeof __typeof__
+#endif
+
 #ifndef MIN
 #define MIN(a, b)                                                              \
 	({                                                                     \
