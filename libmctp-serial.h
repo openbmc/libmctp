994aea13216a8d61947a58bfcf121e2bafa52887
Andrew Jeffery <andrew@aj.id.au>
2022-07-25 20:44:39 +0930

    serial: Add mctp_serial_init_pollfd()
    This continues the transition away from the *_get_fd() calls to
    *_init_pollfd(), allowing the binding to own the poll(2) parameters.
    
    Completely transitioning the binding to async behaviour requires some
    careful work to remove the mctp_write_all() macro from the depths of the
    implementation. That's not attempted here, and the new API doesn't
    regress the current behaviour.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I4e1d05b75a89151e2b469c93de2ef75bc44a9233

--- libmctp-serial.h
+++ libmctp-serial.h
@@ -18,6 +18,11 @@ struct mctp_binding *mctp_binding_serial_core(struct mctp_binding_serial *b);
 
 /* file-based IO */
 int mctp_serial_get_fd(struct mctp_binding_serial *serial);
+
+struct pollfd;
+int mctp_serial_init_pollfd(struct mctp_binding_serial *serial,
+			    struct pollfd *pollfd);
+
 int mctp_serial_read(struct mctp_binding_serial *serial);
 int mctp_serial_open_path(struct mctp_binding_serial *serial,
 		const char *path);
