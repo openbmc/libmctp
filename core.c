5805267a4fcd476b4adec17d5340e523dfe8a726
Rashmica Gupta <rashmica@linux.ibm.com>
2022-11-07 16:34:50 +1100

    pcap: Use SLL2 linktype for captures
    This means we can use wireshark on our pcaps.
    
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
    Change-Id: I215af00e064d352e41d0a8eb87d5a30582998676

--- core.c
+++ core.c
@@ -543,7 +543,7 @@ void mctp_bus_rx(struct mctp_binding *binding, struct mctp_pktbuf *pkt)
 		goto out;
 
 	if (binding->capture)
-		binding->capture(pkt, binding->capture_data);
+		binding->capture(pkt, false, binding->capture_data);
 
 	hdr = mctp_pktbuf_hdr(pkt);
 
@@ -677,7 +677,7 @@ static int mctp_packet_tx(struct mctp_bus *bus,
 		return -1;
 
 	if (binding->capture)
-		binding->capture(pkt, binding->capture_data);
+		binding->capture(pkt, true, binding->capture_data);
 
 	return bus->binding->tx(bus->binding, pkt);
 }
